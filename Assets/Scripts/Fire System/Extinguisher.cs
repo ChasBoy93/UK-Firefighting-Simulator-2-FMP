11357914d4939e6d60615ec9ecf762c0841291e9
Charlie Wood <549750@chrome.escg.ac.uk>
2026-03-17 15:53:58 +0000

    Fire System Started

---
+++ Assets/Scripts/Fire System/Extinguisher.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Extinguisher : MonoBehaviour
+{
+
+    [SerializeField] private float amountExtinguishPerSecond = 1.0f;
+    private void Update()
+    {
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 100f) && hit.collider.TryGetComponent(out Fire fire))
+        { 
+            fire.TryExtinguish(amountExtinguishPerSecond * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Fire System/Fire.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    [SerializeField, Range(0f, 1f)] private float currentIntensity = 1.0f;
+    private float[] startIntensities = new float[0];
+    float timeLastWatered = 0;
+    [SerializeField] private float regenDelay = 2.5f;
+    [SerializeField] private float regenRate = 0.1f;
+
+    [SerializeField] private ParticleSystem []firePartcleSystems = new ParticleSystem[0];
+
+    private bool isLit = true;
+
+    private void Start()
+    {
+        startIntensities = new float[firePartcleSystems.Length];
+
+        for(int i = 0; i < firePartcleSystems.Length; i++)
+        {
+            startIntensities[i] = firePartcleSystems[i].emission.rateOverTime.constant;
+        }
+    }
+
+
+
+    private void Update()
+    {
+        if(isLit && currentIntensity < 1.0f && Time.time - timeLastWatered >= regenDelay)
+        {
+            currentIntensity += regenRate * Time.deltaTime;
+            ChangeIntensity();
+        }
+    }
+
+    public bool TryExtinguish(float amount)
+    {
+        timeLastWatered = Time.time;
+
+        currentIntensity -= amount;
+
+        if(currentIntensity < 0f)
+        {
+            isLit = false;
+            return true;
+        }
+
+        ChangeIntensity();
+
+        return false;
+    }
+
+    private void ChangeIntensity()
+    {
+        for (int i = 0; i < firePartcleSystems.Length; i++)
+        {
+            var emission = firePartcleSystems[i].emission;
+            emission.rateOverTime = currentIntensity * startIntensities[i];
+        }
+    }
+}
