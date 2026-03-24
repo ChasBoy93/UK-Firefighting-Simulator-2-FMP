7eaf5f2bc2cbe8cd0a91574b3d0c1ffbd2c34d12
Charlie Wood <549750@chrome.escg.ac.uk>
2026-03-24 15:41:15 +0000

    .

--- Assets/FireStation/Scripts/DoorAnimation.cs
+++ Assets/FireStation/Scripts/DoorAnimation.cs
@@ -1,13 +1,17 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class DoorAnimation : MonoBehaviour
+public class DoorAnimation : MonoBehaviour, IInteractable
 {
 	public Animator animator;
 	public string onTriggerEnterParameterName;
 	public string onTriggerExitParameterName;
 	public AudioClip OpenSound;
 	public AudioClip CloseSound;
+
+	bool isDoorOpen;
 
 
 	void Start()
@@ -22,21 +26,65 @@ public class DoorAnimation : MonoBehaviour
 		}
 	}
 
-	void OnTriggerEnter()
+	public void Interact()
+	{
+		if(isDoorOpen)
+		{
+			CloseDoor();
+		}
+		else
+		{
+			OpenDoor();
+		}
+    }
+
+    public string GetInteractionText()
+    {
+        if (isDoorOpen)
+		{
+            return "Close Pump Bay Door";
+        }
+		
+		return "Open Pump Bay Door";
+    }
+
+
+    /*void OnTriggerEnter()
 	{
 		if(onTriggerEnterParameterName != null)
 		{
 			gameObject.GetComponent<AudioSource>().PlayOneShot(OpenSound);
 			animator.SetTrigger(onTriggerEnterParameterName);
 		}
-	}
+	}*/
 
-	void OnTriggerExit()
+    void OpenDoor()
 	{
-		if(onTriggerExitParameterName != null)
-		{
+
+            gameObject.GetComponent<AudioSource>().PlayOneShot(OpenSound);
+            animator.SetTrigger(onTriggerEnterParameterName);
+            isDoorOpen = true;
+    }
+
+	void CloseDoor()
+	{
 			gameObject.GetComponent<AudioSource>().PlayOneShot(CloseSound);
 			animator.SetTrigger(onTriggerExitParameterName);
-		}
-	}
+			isDoorOpen = false;
+    }
+
+    void OnTriggerExit()
+	{
+        if (tag == "Fire Appliance" && onTriggerExitParameterName != null)
+		{
+            StartCoroutine(CloseTheDoor());
+        }
+    } 
+
+	IEnumerator CloseTheDoor()
+	{
+		yield return new WaitForSeconds(5);
+        gameObject.GetComponent<AudioSource>().PlayOneShot(CloseSound);
+        animator.SetTrigger(onTriggerExitParameterName);
+    }
 }
