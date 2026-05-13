eeeea2c819971c3eeb26bb526bc88eefe11c9976
Charlie Wood <549750@chrome.escg.ac.uk>
2026-05-13 11:58:07 +0100

    Bugs Fixed

--- Assets/Scripts/Fire System/Extinguisher.cs
+++ Assets/Scripts/Fire System/Extinguisher.cs
@@ -8,9 +8,10 @@ public class Extinguisher : MonoBehaviour
     [SerializeField] private float amountExtinguishPerSecond = 1.0f;
     private void Update()
     {
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 200f) && hit.collider.TryGetComponent(out Fire fire))
-        { 
-            fire.TryExtinguish(amountExtinguishPerSecond * Time.deltaTime);
-        }
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 200f) && hit.collider.TryGetComponent(out Fire fire))
+            {
+                fire.TryExtinguish(amountExtinguishPerSecond * Time.deltaTime);
+            }
+
     }
 }
