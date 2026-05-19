a353c4f8c63f1c8b9f537c93aae369a82b2ce105
Charlie Wood <549750@chrome.escg.ac.uk>
2026-05-19 21:29:05 +0100

    Graphic Quality Complete

--- Assets/Scripts/Menus/Main Menu/SetQuality.cs
+++ Assets/Scripts/Menus/Main Menu/SetQuality.cs
@@ -5,8 +5,23 @@ public class SetQuality : MonoBehaviour
 {
     [SerializeField] private TMP_Dropdown qualityDropdown;
 
+    private const string QUALITY_PREF_KEY = "QualityLevel";
+
+    private void Start()
+    {
+        int savedQuality = PlayerPrefs.GetInt(QUALITY_PREF_KEY, QualitySettings.GetQualityLevel());
+
+        QualitySettings.SetQualityLevel(savedQuality, false);
+
+        qualityDropdown.value = savedQuality;
+        qualityDropdown.RefreshShownValue();
+    }
+
     public void SetQualityLevelDropdown(int index)
     {
         QualitySettings.SetQualityLevel(index, false);
+
+        PlayerPrefs.SetInt(QUALITY_PREF_KEY, index);
+        PlayerPrefs.Save();
     }
 }
